446c56216ff983e2fb3c355ee4579c8786b70ab1
Michele Tagliabue <michele.tagliabue@unox.com>
2022-06-13 11:35:14 +0200

    Removed logs -> layout change fixed

--- src/lib/components/Keyboard.tsx
+++ src/lib/components/Keyboard.tsx
@@ -3,9 +3,12 @@ import Keyboard from "simple-keyboard";
 import { parseProps, changedProps } from "../services/Utilities";
 import "simple-keyboard/build/css/index.css";
 import { KeyboardReactInterface } from "../interfaces.d";
+import { useState } from "react";
 
 const KeyboardReact = (props: KeyboardReactInterface["options"]) => {
-  const cssClass = props.baseClass || "react-simple-keyboard";
+  const [cssClass, setCssClass] = useState(
+    props.baseClass || "react-simple-keyboard"
+  );
   const initRef = React.useRef<null | boolean>(null);
   const targetElemRef = React.useRef<null | HTMLDivElement>(null);
   const keyboardRef = React.useRef<null | KeyboardReactInterface>(null);
@@ -17,31 +20,33 @@ const KeyboardReact = (props: KeyboardReactInterface["options"]) => {
 
     if (
       updatedProps.find((f) => f === "baseClass") !== undefined &&
-      keyboardRef.current
+      keyboardRef.current &&
+      initRef.current === true
     ) {
-      console.log("baseClass changed");
       // If base class is changed, I need to rerender from scratch the keyboard since
       // baseClass is used by the keyboard to generate instance reference
       keyboardRef.current.destroy();
       initRef.current = false;
+      setCssClass(parsedProps["baseClass"]);
+      return;
     }
     /**
      * Initialize simple-keyboard
      */
     if (!initRef.current) {
-      console.log("Initializing simple-keyboard");
       initRef.current = true;
-      console.log("ReactSimpleKeyboard: Init");
+      parsedProps.debug && console.log("ReactSimpleKeyboard: Init");
       const targetElem = targetElemRef.current as HTMLDivElement;
       const targetClass = `.${cssClass}`;
-      console.log("TargetElement:", targetElem, "TargetClass:", targetClass);
+      parsedProps.debug &&
+        console.log("TargetElement:", targetElem, "TargetClass:", targetClass);
       keyboardRef.current = new Keyboard(
         targetElem || targetClass,
         parsedProps
       ) as KeyboardReactInterface;
       parsedProps.keyboardRef && parsedProps.keyboardRef(keyboardRef.current);
     } else {
-      console.log("SimpleKeyboard already initialized");
+      parsedProps.debug && console.log("SimpleKeyboard already initialized");
     }
 
     /**
