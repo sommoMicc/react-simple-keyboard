37329a7e7f26e310dbbeb725c0d208533418faae
Michele Tagliabue <michele.tagliabue@unox.com>
2023-06-12 12:18:04 +0200

    a

--- build/components/KeyboardModern.d.ts
+++ build/components/KeyboardModern.d.ts
@@ -1,5 +1,5 @@
-/// <reference types="react" />
+import * as React from "react";
 import "simple-keyboard/build/css/index.css";
 import { KeyboardReactInterface } from "../interfaces";
-declare const KeyboardReact: (props: KeyboardReactInterface["options"]) => JSX.Element;
+declare const KeyboardReact: (props: KeyboardReactInterface["options"]) => React.JSX.Element;
 export default KeyboardReact;
